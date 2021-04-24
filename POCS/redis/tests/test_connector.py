c6bf525ce5bb5388bc3a4855d6edb8c9db047268
Rodrigo Garcia <Rodrigo.Garcia@jala-foundation.org>
2021-04-24 14:52:00 -0400

    Fix test hset hget and pipeline

--- POCS/redis/tests/test_connector.py
+++ POCS/redis/tests/test_connector.py
@@ -1,5 +1,7 @@
 from connector import RedisConnector
 import unittest
+import json
+
 def test_1():
     assert True
 
@@ -14,34 +16,41 @@ class TestRedisConnector(unittest.TestCase):
         res = r.get('ding').decode('utf-8')
         self.assertEqual(res, 'DONG!')
 
+    def test_set_serialized(self):
+        r = RedisConnector().get_connection()
+        d = {
+            'lista': [1,2,4,'c'],
+            'clista': [-5,23, 2.4, 'com'],
+            'dd': {'almacen1': 'San Diego'}
+        }
+        r.set('obj1', json.dumps(d))
+        self.assertEqual(r.get('obj1').decode('utf-8'), json.dumps(d))
+        #print('GET')
+        #print(r.get('obj1'))
+        #print(r.get('obj1').decode('utf-8'))
+
     def test_iterate(self):
         r = RedisConnector().get_connection()
         for key in r.scan_iter():
-            print(f'>>>> {key}')
-            print(r.exists(key))
-            print(key, r.mget(key))
+            #print(f'>>>> {key}')
+            #print(r.exists(key))
+            #print(key, r.mget(key))
             self.assertNotEqual(r.mget(key), None)
 
-    def test_pipeline(self):
+    def test_hset_and_pipeline(self):
         r = RedisConnector().get_connection()
         regs = {
-            'gf1': {'Do': 'Done', 'Done': 51},
-            'gf2': {'Do': 'Chess', 'Done': 77},
-            'gf3': {'Do': 'Swipe', 'Done': 824},
+            'gf1': 'Done',
+            'gf2': 'Chess',
+            'gf3': 'Swipe',
         }
         with r.pipeline() as pipe:
             for key, value in regs.items():
-                pipe.hmset(key, value)
+                pipe.hset('hsetv1', key, value)
             pipe.execute()
             self.assertEqual(r.bgsave(), True)
 
-        for key, value in regs.items():
-            resp = r.hgetall(key)
-            print("lllllllllllll")
-            print(resp)
-            self.assertNotEqual(resp, None)
-            for k1, v1 in regs[key].items():
-                print(f'{k1}: {v1}')
-                print(resp, type(resp))
-                print(resp.get(k1, '0000>0'))
-                self.assertEqual(resp[k1] == v1)
+        for field, value in regs.items():
+            resp = r.hget('hsetv1', field)
+            print(f'>> {resp} {resp.decode("utf-8")} {regs[field]}')
+            self.assertEqual(resp.decode('utf-8'), regs[field])

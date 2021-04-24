149d13d382872b4688b2025540074429f16e892a
Rodrigo Garcia <Rodrigo.Garcia@jala-foundation.org>
2021-04-24 15:13:11 -0400

    Added tests set exipry

--- POCS/redis/tests/test_connector.py
+++ POCS/redis/tests/test_connector.py
@@ -1,6 +1,8 @@
 from connector import RedisConnector
 import unittest
 import json
+from datetime import timedelta
+import time
 
 def test_1():
     assert True
@@ -15,19 +17,6 @@ class TestRedisConnector(unittest.TestCase):
         r.mset({'ding': 'DONG!'})
         res = r.get('ding').decode('utf-8')
         self.assertEqual(res, 'DONG!')
-
-    def test_set_serialized(self):
-        r = RedisConnector().get_connection()
-        d = {
-            'lista': [1,2,4,'c'],
-            'clista': [-5,23, 2.4, 'com'],
-            'dd': {'almacen1': 'San Diego'}
-        }
-        r.set('obj1', json.dumps(d))
-        self.assertEqual(r.get('obj1').decode('utf-8'), json.dumps(d))
-        #print('GET')
-        #print(r.get('obj1'))
-        #print(r.get('obj1').decode('utf-8'))
 
     def test_iterate(self):
         r = RedisConnector().get_connection()
@@ -52,5 +41,29 @@ class TestRedisConnector(unittest.TestCase):
 
         for field, value in regs.items():
             resp = r.hget('hsetv1', field)
-            print(f'>> {resp} {resp.decode("utf-8")} {regs[field]}')
+            #print(f'>> {resp} {resp.decode("utf-8")} {regs[field]}')
             self.assertEqual(resp.decode('utf-8'), regs[field])
+
+    def test_set_serialized(self):
+        r = RedisConnector().get_connection()
+        d = {
+            'lista': [1,2,4,'c'],
+            'clista': [-5,23, 2.4, 'com'],
+            'dd': {'almacen1': 'San Diego'}
+        }
+        r.set('obj1', json.dumps(d))
+        self.assertEqual(r.get('obj1').decode('utf-8'), json.dumps(d))
+        #print('GET')
+        #print(r.get('obj1'))
+        #print(r.get('obj1').decode('utf-8'))
+
+    def test_key_expiry(self):
+        r = RedisConnector().get_connection()
+        r.setex("runner", timedelta(seconds=2), value="exists")
+        #print(r.get("runner"))
+        #print(r.ttl("runner"), r.pttl("runner"))
+        self.assertEqual(r.get("runner").decode('utf-8'), "exists")
+        time.sleep(1)
+        # time to live should have decreased some value
+        self.assertLess(r.pttl("runner"), 2000)
+        #self.assertEqual(r.pttl("runner"), )
